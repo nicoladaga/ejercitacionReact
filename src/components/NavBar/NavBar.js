e8dfde290c9df5c8b195193d6396be551b9b9585
NicoLadaga <nicoo.ladaga@gmail.com>
2022-08-28 23:15:37 -0300

    ejercicio componentes uno

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import logo from '../../logoSinTexto.png';
 import './NavBar.css';
+import CartWidget from '../CartWidget/CartWidget';
 
 const NavBar = () => {
     return (
@@ -11,7 +12,7 @@ const NavBar = () => {
                 <ul className='navbar-list'>
                     <li className='navbar-list-element'><a href="#">Inicio</a></li>
                     <li className='navbar-list-element'><a href="#">Productos</a></li>
-                    <li className='navbar-list-element'><a href="#">Carrito</a></li>
+                    <li className='navbar-list-element'><a href="#"><CartWidget /></a></li>
                 </ul>
             </div>
             <div className='navbar-login'><button>Login</button></div>
